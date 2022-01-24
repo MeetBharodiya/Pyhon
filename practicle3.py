ad29b27f5e39ccaaac312ab689a9f429b417ace8
Meet Bharodiya <97339113+MeetBharodiya@users.noreply.github.com>
2022-01-24 23:07:53 +0530

    Final code

---
+++ practicle3.py
@@ -0,0 +1,13 @@
+k=input()
+
+# Take input as string and convert it into list
+str=input()
+lst=list(str.split(' '))
+
+for i in lst:
+    # count function will return frequncy of i in list
+    curr_frequency=lst.count(i);
+    if curr_frequency==1:
+        captain_room=i;
+print(captain_room);
+
